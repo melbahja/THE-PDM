df480d4ce60a56431d5cecd78821b51e8cdffe78
Mohamed Elbahja <melbahja@users.noreply.github.com>
2016-11-22 14:15:40 +0000

    Update examples.php

--- examples.php
+++ examples.php
@@ -22,3 +22,19 @@ $dml->start();
  * table name is : xprefix_users
  */
 $users = $dml->getUsers();
+
+
+/** Left Join **/
+//$data = $dml->select('@prefix_tableName.id, @prefix_tableNameTwo.user', 'tableName', 'LEFT JOIN @prefix_tableNameTwo ON @prefix_tableName.id=@prefix_tableNameTwo.uid');
+
+
+/** DDL **/
+$ddl = $pdm->load('DDL', 'Database1');
+$ddl->start();
+
+$c = $ddl->create('tableName', array(
+    'id'          => 'int NOT NULL PRIMARY KEY',
+    'name'        => 'varchar(122) COLLATE @charset_general_ci NOT NULL',
+    'username'    => 'varchar(50) COLLATE @charset_general_ci NOT NULL')); // @charset = default charset in config file
+
+var_dump($c);
